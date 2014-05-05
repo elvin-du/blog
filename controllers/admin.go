355a97f091eeb3a5cc1bc170cfa132791d3dc97d
nick Du <nick@yun.io>
2014-05-05 20:52:56 +0800

    add amdin

--- controllers/admin.go
+++ controllers/admin.go
@@ -8,7 +8,7 @@ type AdminController struct {
 
 func (this *AdminController) Index() {
 	this.Layout = "layout.html"
-	this.TplNames = "admin/index.html"
+	this.TplNames = "admin/login.html"
 	this.LayoutSections = make(map[string]string)
 	this.LayoutSections["Nav"] = "admin/nav.html"
 	this.LayoutSections["CSS"] = "admin/css.html"
@@ -17,5 +17,19 @@ func (this *AdminController) Index() {
 
 //Method-POST; URL-/admin/login
 func (this *AdminController) Login() {
+	//name := this.GetString("name")
+	//passwd := this.GetString("passwd")
 
+	//cookie, err := AdminModel().Login(name, passwd)
+	//if nil != err {
+	//	beego.Error(err)
+	//	return
+	//}
+
+	//sess := globalSessions.SessionStart(this.Ctx.ResponseWriter, this.Ctx.Request)
+	//defer sess.SessionRelease()
+	//sess.Set()
+
+	//this.Ctx.SetCookie(C_COOKIE_NAME, cookie)
+	//this.Redirect("/admin", C_HTTP_OK)
 }
--- models/admin.go
+++ models/admin.go
@@ -1,6 +1,6 @@
 package models
 
-import _ "github.com/go-sql-driver/mysql" // import your used driver
+import _ "github.com/go-sql-driver/mysql"
 
 type admin byte
 
@@ -9,3 +9,27 @@ var _gAdmin admin = ADMIN_MODEL
 func AdminModel() *admin {
 	return &_gAdmin
 }
+
+//@RET:cookie
+//func (this *admin) Login(name, passwd string) (string, error) {
+//	h := md5.New()
+//	_, err := h.Write([]byte(passwd))
+//	if nil != err {
+//		beego.Error(err)
+//		return "", err
+//	}
+//	passwd = hex.EncodeToString(h.Sum(nil))
+
+//	o := orm.NewOrm()
+//	var maps []orm.Params
+//	num, err := o.Raw("select count(1) from blog.admins where name = ? and password = ?", name, passwd).Values(&maps)
+//	if nil != err {
+//		beego.Error(err)
+//		return "", err
+//	}
+//	if num <= 0 {
+//		return "", E_NOT_FOUND
+//	}
+
+//	return
+//}
